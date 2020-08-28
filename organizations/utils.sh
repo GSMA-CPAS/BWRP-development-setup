a99cc89af11a76e89421e7c61d6cb56e81dfb7e1
Sebastian Meyer zu Borgsen <sebastian.meyer-zu-borgsen@telekom.de>
2020-08-28 12:42:08 +0000

    make functions of tuils directly callable

--- organizations/utils.sh
+++ organizations/utils.sh
@@ -181,3 +181,14 @@ signConfig() {
   res=$?
   set +x
 }
+
+# Check if the function exists (bash specific)
+if declare -f "$1" > /dev/null
+then
+  # call arguments verbatim
+  "$@"
+else
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
