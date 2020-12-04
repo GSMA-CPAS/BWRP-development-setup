568fb8169f3336d980c514a032b48ce63b1aa466
zkong <zkong@gsma.com>
2020-12-04 13:09:58 -0500

    Added common-adapter test script

--- nomad.sh
+++ nomad.sh
@@ -210,8 +210,8 @@ function build() {
         ]
     }
 );
-db.contract.createIndex({ id: 1 }, { unique: true });
-db.contract.createIndex({documentId: 1}, {unique: true, partialFilterExpression: {documentId: {\$type: 'string'}}});
+db.contracts.createIndex({ id: 1 }, { unique: true });
+db.contracts.createIndex({documentId: 1}, {unique: true, partialFilterExpression: {documentId: {\$type: 'string'}}});
 " > ${DTAG_MONGO_PV_PATH}mongo-init.js
 
   mkdir -p ${TMUS_MONGO_PV_PATH}
@@ -227,8 +227,8 @@ db.contract.createIndex({documentId: 1}, {unique: true, partialFilterExpression:
         ]
     }
 );
-db.contract.createIndex({ id: 1 }, { unique: true });
-db.contract.createIndex({documentId: 1}, {unique: true, partialFilterExpression: {documentId: {\$type: 'string'}}});
+db.contracts.createIndex({ id: 1 }, { unique: true });
+db.contracts.createIndex({documentId: 1}, {unique: true, partialFilterExpression: {documentId: {\$type: 'string'}}});
 " > ${TMUS_MONGO_PV_PATH}mongo-init.js
 }
 
