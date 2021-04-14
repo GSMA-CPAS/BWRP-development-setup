605acba724df39d5b91512fa8900183149f81d6c
askimina <adam.skimina@ovoo.pl>
2021-04-14 08:32:15 -0400

    Discrepancy-service integration

--- nomad.sh
+++ nomad.sh
@@ -243,7 +243,7 @@ function up() {
 }
 
 function down() {
-  filter='hyperledger\|dev-peer\|mysql\|nginx\|webapp\|offchain\|blockchain-adapter\|cadb\|common-adapter\|calculator-service'
+  filter='hyperledger\|dev-peer\|mysql\|nginx\|webapp\|offchain\|blockchain-adapter\|cadb\|common-adapter\|calculator-service\|discrepancy-service'
   #docker stop $(docker ps -a -q)
   docker stop $(docker ps -a | grep $filter | awk '{print $1}')
   #docker kill $(docker ps -q)
